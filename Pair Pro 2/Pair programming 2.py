9ac1c78ac9f793d9dc8e7dec40dba23b8dc96b7f
Alec Dubovyk <91628750+AlecDubov@users.noreply.github.com>
2021-10-27 11:09:38 -0700

    more files
    more

---
+++ Pair Pro 2/Pair programming 2.py
@@ -0,0 +1,29 @@
+import turtle 
+screen = turtle.Screen()
+
+image="Jump.png"
+
+screen.addshape(image)
+turtle.__file__("image")
+
+turtle.mainloop()
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
